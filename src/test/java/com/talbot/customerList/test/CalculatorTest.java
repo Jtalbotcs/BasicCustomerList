387dfe71b1ca970516e2d8cf65b2669134774a42
Jason Talbot <jtalbotcs@gmail.com>
2017-02-17 17:24:01 -0500

    Integrated JUnit with spring

--- src/test/java/com/talbot/customerList/test/CalculatorTest.java
+++ src/test/java/com/talbot/customerList/test/CalculatorTest.java
@@ -1,8 +1,13 @@
 package com.talbot.customerList.test;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.junit.Assert.*;
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:customerList-servlet.xml"})
 public class CalculatorTest {
 	@Test
 	public void testAbs()
